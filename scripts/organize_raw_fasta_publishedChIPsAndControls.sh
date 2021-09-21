383dab10743f4b193a3f4598d8a563904f928141
sklasfeld <sjk314@gmail.com>
2021-09-21 10:05:43 -0400

    fix TFL1_S experiments, remove old files, and check for raw files before organizing

--- scripts/organize_raw_fasta_publishedChIPsAndControls.sh
+++ scripts/organize_raw_fasta_publishedChIPsAndControls.sh
@@ -1,51 +1,306 @@
-#!/bin/bash
-
-gunzip -c SRR10676619.fastq > fastq/raw/FD_W_2020_R1.fastq.gz
-gunzip -c SRR10676620.fastq > fastq/raw/FD_W_2020_R2.fastq.gz
-gunzip -c SRR10676621.fastq > fastq/raw/TFL1_A_W_2020_R1.fastq.gz
-gunzip -c SRR10676622.fastq > fastq/raw/TFL1_A_W_2020_R2.fastq.gz
-gunzip -c SRR10676623.fastq > fastq/raw/TFL1_B_W_2020_R1.fastq.gz
-gunzip -c SRR10676624.fastq > fastq/raw/TFL1_B_W_2020_R2.fastq.gz
-gunzip -c SRR10676625.fastq > fastq/raw/TFL1_fd_W_2020_R1.fastq.gz
-gunzip -c SRR10676626.fastq > fastq/raw/TFL1_fd_W_2020_R2.fastq.gz
-gunzip -c SRR10676631.fastq > fastq/raw/TFL1_FD_W_2020_Input_R1.fastq.gz
-gunzip -c SRR10676632.fastq > fastq/raw/TFL1_FD_W_2020_Input_R2.fastq.gz
-gunzip -c SRR10676633.fastq > fastq/raw/TFL1_FD_W_2020_Input_R3.fastq.gz
-gunzip -c SRR10676634.fastq > fastq/raw/TFL1_FD_W_2020_Input_R4.fastq.gz
-gunzip -c SRR10676627.fastq > fastq/raw/TFL1_FD_W_2020_Mock_R1.fastq.gz
-gunzip -c SRR10676628.fastq > fastq/raw/TFL1_FD_W_2020_Mock_R2.fastq.gz
-gunzip -c SRR10676629.fastq > fastq/raw/TFL1_fd_W_2020_Mock_R1.fastq.gz
-gunzip -c SRR10676630.fastq > fastq/raw/TFL1_fd_W_2020_Mock_R2.fastq.gz
-gunzip -c SRR10614284.fastq > fastq/raw/LFY_W_2021_R1.fastq.gz
-gunzip -c SRR10614285.fastq > fastq/raw/LFY_W_2021_R2.fastq.gz
-gunzip -c SRR10614286.fastq > fastq/raw/LFY_W_2021_R3.fastq.gz
-gunzip -c SRR10614278.fastq > fastq/raw/LFY_W_2021_Input_R1.fastq.gz
-gunzip -c SRR10614279.fastq > fastq/raw/LFY_W_2021_Input_R2.fastq.gz
-gunzip -c SRR10614280.fastq > fastq/raw/LFY_W_2021_Input_R3.fastq.gz
-gunzip -c SRR10614281.fastq > fastq/raw/LFY_W_2021_Mock_R1.fastq.gz
-gunzip -c SRR10614282.fastq > fastq/raw/LFY_W_2021_Mock_R2.fastq.gz
-gunzip -c SRR10614283.fastq > fastq/raw/LFY_W_2021_Mock_R3.fastq.gz
-gunzip -c SRR1719111.fastq > fastq/raw/LFY_P_2016_R1.fastq.gz
-gunzip -c SRR1719112.fastq > fastq/raw/LFY_P_2016_R2.fastq.gz
-gunzip -c SRR1719115.fastq > fastq/raw/LFY_P_2016_Mock_R1.fastq.gz
-gunzip -c SRR1719116.fastq > fastq/raw/LFY_P_2016_Mock_R2.fastq.gz
-mv ERR2286784.fastq.gz fastq/raw/FD_S_2019_R1.fastq.gz
-mv ERR2286785.fastq.gz fastq/raw/FD_S_2019_R2.fastq.gz
-mv ERR2286786.fastq.gz fastq/raw/FD_ft10_tsf1_S_2019_R1.fastq.gz
-mv ERR2286787.fastq.gz fastq/raw/FD_ft10_tsf1_S_2019_R2.fastq.gz
-mv ERR2286788.fastq.gz fastq/raw/FD_S_2019_Mock_R1.fastq.gz
-mv ERR2286789.fastq.gz fastq/raw/FD_ft10_tsf1_S_2019_Mock_R1.fastq.gz
-mv ERR2820083.fastq.gz fastq/raw/TFL1_S_2020_R1.fastq.gz
-mv ERR2820084.fastq.gz fastq/raw/TFL1_S_2020_R2.fastq.gz
-mv ERR2820085.fastq.gz fastq/raw/TFL1_S_2020_R3.fastq.gz
-mv ERR2820080.fastq.gz fastq/raw/TFL1_S_2020_Mock_R1.fastq.gz
-mv ERR2820081.fastq.gz fastq/raw/TFL1_S_2020_Mock_R2.fastq.gz
-mv ERR2820082.fastq.gz fastq/raw/TFL1_S_2020_Mock_R3.fastq.gz
-gunzip -c SRR070384.fastq > fastq/raw/LFY_P_2011_R1.fastq.gz
-gunzip -c SRR070385.fastq > fastq/raw/LFY_P_2011_R2.fastq.gz
-gunzip -c SRR070382.fastq > fastq/raw/LFY_P_2011_Input_R1.fastq.gz
-gunzip -c SRR070383.fastq > fastq/raw/LFY_P_2011_Input_R2.fastq.gz
-gunzip -c SRR9961307.fastq > fastq/raw/FD_C_2020_R1.fastq.gz
-gunzip -c SRR9961292.fastq > fastq/raw/FD_C_2020_R2.fastq.gz
-gunzip -c SRR9961306.fastq > fastq/raw/FD_C_2020_Input_R1.fastq.gz
-gunzip -c SRR9961293.fastq > fastq/raw/FD_C_2020_Input_R2.fastq.gz
+raw_file=""
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	#!/bin/bash
+fi
+raw_file=""
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	
+fi
+raw_file="SRR10676619.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR10676619.fastq > fastq/raw/FD_W_2020_R1.fastq.gz && rm SRR10676619.fastq
+fi
+raw_file="SRR10676620.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR10676620.fastq > fastq/raw/FD_W_2020_R2.fastq.gz && rm SRR10676620.fastq
+fi
+raw_file="SRR10676621.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR10676621.fastq > fastq/raw/TFL1_A_W_2020_R1.fastq.gz && rm SRR10676621.fastq
+fi
+raw_file="SRR10676622.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR10676622.fastq > fastq/raw/TFL1_A_W_2020_R2.fastq.gz && rm SRR10676622.fastq
+fi
+raw_file="SRR10676623.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR10676623.fastq > fastq/raw/TFL1_B_W_2020_R1.fastq.gz && rm SRR10676623.fastq
+fi
+raw_file="SRR10676624.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR10676624.fastq > fastq/raw/TFL1_B_W_2020_R2.fastq.gz && rm SRR10676624.fastq
+fi
+raw_file="SRR10676625.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR10676625.fastq > fastq/raw/TFL1_fd_W_2020_R1.fastq.gz && rm SRR10676625.fastq
+fi
+raw_file="SRR10676626.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR10676626.fastq > fastq/raw/TFL1_fd_W_2020_R2.fastq.gz && rm SRR10676626.fastq
+fi
+raw_file="SRR10676631.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR10676631.fastq > fastq/raw/TFL1_FD_W_2020_Input_R1.fastq.gz && rm SRR10676631.fastq
+fi
+raw_file="SRR10676632.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR10676632.fastq > fastq/raw/TFL1_FD_W_2020_Input_R2.fastq.gz && rm SRR10676632.fastq
+fi
+raw_file="SRR10676633.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR10676633.fastq > fastq/raw/TFL1_FD_W_2020_Input_R3.fastq.gz && rm SRR10676633.fastq
+fi
+raw_file="SRR10676634.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR10676634.fastq > fastq/raw/TFL1_FD_W_2020_Input_R4.fastq.gz && rm SRR10676634.fastq
+fi
+raw_file="SRR10676627.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR10676627.fastq > fastq/raw/TFL1_FD_W_2020_Mock_R1.fastq.gz && rm SRR10676627.fastq
+fi
+raw_file="SRR10676628.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR10676628.fastq > fastq/raw/TFL1_FD_W_2020_Mock_R2.fastq.gz && rm SRR10676628.fastq
+fi
+raw_file="SRR10676629.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR10676629.fastq > fastq/raw/TFL1_fd_W_2020_Mock_R1.fastq.gz && rm SRR10676629.fastq
+fi
+raw_file="SRR10676630.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR10676630.fastq > fastq/raw/TFL1_fd_W_2020_Mock_R2.fastq.gz && rm SRR10676630.fastq
+fi
+raw_file="SRR10614284.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR10614284.fastq > fastq/raw/LFY_W_2021_R1.fastq.gz && rm SRR10614284.fastq
+fi
+raw_file="SRR10614285.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR10614285.fastq > fastq/raw/LFY_W_2021_R2.fastq.gz && rm SRR10614285.fastq
+fi
+raw_file="SRR10614286.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR10614286.fastq > fastq/raw/LFY_W_2021_R3.fastq.gz && rm SRR10614286.fastq
+fi
+raw_file="SRR10614278.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR10614278.fastq > fastq/raw/LFY_W_2021_Input_R1.fastq.gz && rm SRR10614278.fastq
+fi
+raw_file="SRR10614279.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR10614279.fastq > fastq/raw/LFY_W_2021_Input_R2.fastq.gz && rm SRR10614279.fastq
+fi
+raw_file="SRR10614280.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR10614280.fastq > fastq/raw/LFY_W_2021_Input_R3.fastq.gz && rm SRR10614280.fastq
+fi
+raw_file="SRR10614281.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR10614281.fastq > fastq/raw/LFY_W_2021_Mock_R1.fastq.gz && rm SRR10614281.fastq
+fi
+raw_file="SRR10614282.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR10614282.fastq > fastq/raw/LFY_W_2021_Mock_R2.fastq.gz && rm SRR10614282.fastq
+fi
+raw_file="SRR10614283.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR10614283.fastq > fastq/raw/LFY_W_2021_Mock_R3.fastq.gz && rm SRR10614283.fastq
+fi
+raw_file="SRR1719111.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR1719111.fastq > fastq/raw/LFY_P_2016_R1.fastq.gz && rm SRR1719111.fastq
+fi
+raw_file="SRR1719112.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR1719112.fastq > fastq/raw/LFY_P_2016_R2.fastq.gz && rm SRR1719112.fastq
+fi
+raw_file="SRR1719115.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR1719115.fastq > fastq/raw/LFY_P_2016_Mock_R1.fastq.gz && rm SRR1719115.fastq
+fi
+raw_file="SRR1719116.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR1719116.fastq > fastq/raw/LFY_P_2016_Mock_R2.fastq.gz && rm SRR1719116.fastq
+fi
+raw_file="ERR2286784.fastq.gz"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	mv ERR2286784.fastq.gz fastq/raw/FD_S_2019_R1.fastq.gz
+fi
+raw_file="ERR2286785.fastq.gz"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	mv ERR2286785.fastq.gz fastq/raw/FD_S_2019_R2.fastq.gz
+fi
+raw_file="ERR2286786.fastq.gz"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	mv ERR2286786.fastq.gz fastq/raw/FD_ft10_tsf1_S_2019_R1.fastq.gz
+fi
+raw_file="ERR2286787.fastq.gz"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	mv ERR2286787.fastq.gz fastq/raw/FD_ft10_tsf1_S_2019_R2.fastq.gz
+fi
+raw_file="ERR2286788.fastq.gz"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	mv ERR2286788.fastq.gz fastq/raw/FD_S_2019_Mock_R1.fastq.gz
+fi
+raw_file="ERR2286789.fastq.gz"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	mv ERR2286789.fastq.gz fastq/raw/FD_ft10_tsf1_S_2019_Mock_R1.fastq.gz
+fi
+raw_file="ERR2820083.fastq.gz"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	mv ERR2820083.fastq.gz fastq/raw/TFL1_S_2020_R1.fastq.gz
+fi
+raw_file="ERR2820084.fastq.gz"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	mv ERR2820084.fastq.gz fastq/raw/TFL1_S_2020_R2.fastq.gz
+fi
+raw_file="ERR2820085.fastq.gz"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	mv ERR2820085.fastq.gz fastq/raw/TFL1_S_2020_R3.fastq.gz
+fi
+raw_file="ERR2820080.fastq.gz"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	mv ERR2820080.fastq.gz fastq/raw/TFL1_S_2020_Mock_R1.fastq.gz
+fi
+raw_file="ERR2820081.fastq.gz"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	mv ERR2820081.fastq.gz fastq/raw/TFL1_S_2020_Mock_R2.fastq.gz
+fi
+raw_file="ERR2820082.fastq.gz"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	mv ERR2820082.fastq.gz fastq/raw/TFL1_S_2020_Mock_R3.fastq.gz
+fi
+raw_file="SRR070384.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR070384.fastq > fastq/raw/LFY_P_2011_R1.fastq.gz && rm SRR070384.fastq
+fi
+raw_file="SRR070385.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR070385.fastq > fastq/raw/LFY_P_2011_R2.fastq.gz && rm SRR070385.fastq
+fi
+raw_file="SRR070382.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR070382.fastq > fastq/raw/LFY_P_2011_Input_R1.fastq.gz && rm SRR070382.fastq
+fi
+raw_file="SRR070383.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR070383.fastq > fastq/raw/LFY_P_2011_Input_R2.fastq.gz && rm SRR070383.fastq
+fi
+raw_file="SRR9961307.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR9961307.fastq > fastq/raw/FD_C_2020_R1.fastq.gz && rm SRR9961307.fastq
+fi
+raw_file="SRR9961292.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR9961292.fastq > fastq/raw/FD_C_2020_R2.fastq.gz && rm SRR9961292.fastq
+fi
+raw_file="SRR9961306.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR9961306.fastq > fastq/raw/FD_C_2020_Input_R1.fastq.gz && rm SRR9961306.fastq
+fi
+raw_file="SRR9961293.fastq"
+if [ ! -f "$raw_file"]; then
+	echo "ERROR: File ${raw_file} does not exist"
+else
+	gzip -c SRR9961293.fastq > fastq/raw/FD_C_2020_Input_R2.fastq.gz && rm SRR9961293.fastq
+fi
