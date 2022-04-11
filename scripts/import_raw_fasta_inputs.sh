1598db308f0edf905e43e5429210855f9f46360c
Samantha Klasfeld <sjk314@gmail.com>
2022-04-11 16:08:16 -0400

    Update import_raw_fasta_inputs.sh
    Added full path to fastq-dump in sratools software

--- scripts/import_raw_fasta_inputs.sh
+++ scripts/import_raw_fasta_inputs.sh
@@ -1,44 +1,46 @@
 #!/bin/bash
 
+sra_tools_dir="/usr/src/sratoolkit.2.11.2-ubuntu64/bin"
+
 #inputA
-fastq-dump SRR8180351
+${sra_tools_dir}/fastq-dump SRR8180351
 #inputB
-fastq-dump SRR402844
+${sra_tools_dir}/fastq-dump SRR402844
 #inputC
-fastq-dump SRR6042861
+${sra_tools_dir}/fastq-dump SRR6042861
 #inputD
-fastq-dump SRR8742331
+${sra_tools_dir}/fastq-dump SRR8742331
 #inputE
-fastq-dump SRR5011169
+${sra_tools_dir}/fastq-dump SRR5011169
 #inputF
-fastq-dump SRR8365023
+${sra_tools_dir}/fastq-dump SRR8365023
 #inputG
-fastq-dump SRR6435271
+${sra_tools_dir}/fastq-dump SRR6435271
 #inputH
-fastq-dump SRR7646290
+${sra_tools_dir}/fastq-dump SRR7646290
 #inputI
-fastq-dump SRR8890665
+${sra_tools_dir}/fastq-dump SRR8890665
 #inputJ
-fastq-dump SRR7224610
-fastq-dump SRR7224611
-fastq-dump SRR7224612
+${sra_tools_dir}/fastq-dump SRR7224610
+${sra_tools_dir}/fastq-dump SRR7224611
+${sra_tools_dir}/fastq-dump SRR7224612
 #inputK
-fastq-dump SRR2085420
+${sra_tools_dir}/fastq-dump SRR2085420
 #inputL
-fastq-dump SRR394081
+${sra_tools_dir}/fastq-dump SRR394081
 #inputM
-fastq-dump SRR5681375
+${sra_tools_dir}/fastq-dump SRR5681375
 #inputN
-fastq-dump SRR6768681
+${sra_tools_dir}/fastq-dump SRR6768681
 #inputO
-fastq-dump SRR6989574
+${sra_tools_dir}/fastq-dump SRR6989574
 #inputP
-fastq-dump SRR7264106
+${sra_tools_dir}/fastq-dump SRR7264106
 #inputQ
-fastq-dump SRR988541
+${sra_tools_dir}/fastq-dump SRR988541
 #inputR
-fastq-dump SRR5813676
+${sra_tools_dir}/fastq-dump SRR5813676
 #inputS
-fastq-dump SRR866865
+${sra_tools_dir}/fastq-dump SRR866865
 #inputT
-fastq-dump SRR1191642
+${sra_tools_dir}/fastq-dump SRR1191642
